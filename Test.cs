62969315b9665e1b5979aec468e43a3167c21bc5
Andrey Bykov <portperm@yandex.ru>
2025-05-09 11:23:45 +0300

    Добавьте файлы проекта.

---
+++ Test.cs
@@ -0,0 +1,28 @@
+﻿/*
+* Лабораторная работа 7. Упражнение 2. Обращение строки  
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utils
+{
+    public class Test
+    {
+        static void Main()
+        {
+            string message;
+
+            // Get an input string 
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+
+            // Reverse the string 
+            Utils.Reverse(ref message);
+
+            // Display the result 
+            Console.WriteLine(message);
+        }
+    }
+}
